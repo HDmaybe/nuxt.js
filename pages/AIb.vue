c023d16bb5a390166ee223a007fd8dcd4c2787fc
윤희건 <yoonheegeon@HGMac.local>
2024-01-10 17:44:40 +0900

    day3

---
+++ pages/AIb.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <Navbar/>
+        <div class="AIimage">
+            <img src="http://intellisys.co.kr/images/voucher/banner.jpg">
+        </div>
+        <Footer/>
+    </div>
+    
+    
+</template>
+
+<script>
+
+import Navbar from '~/components/Navbar.vue';
+import Footer from '~/components/Footer.vue';
+
+export default {
+    components: {
+      Navbar,
+      Footer
+    },
+}
+</script>
+
+<style scoped>
+
+.AIimage{
+    
+    background-size: cover;
+    max-width: 100%;
+}
+
+</style>
